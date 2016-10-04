b975228a05b7b49fb93ab2493b30e8b9d9e0d4ed
James Hsiao <heroms90@gmail.com>
2016-10-04 12:35:54 +0800

    [MIC-92] Extract error handler for akka stream customization and refactor the related codes.

--- app/me/mig/mars/controllers/SettingsController.scala
+++ app/me/mig/mars/controllers/SettingsController.scala
@@ -3,27 +3,29 @@ package me.mig.mars.controllers
 import javax.inject.{Inject, Singleton}
 
 import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
+import akka.stream.Materializer
 import akka.stream.scaladsl.{Sink, Source}
 import me.mig.mars.services.EmailService
 import me.mig.mars.services.EmailService._
+import play.api.libs.json.{JsError, JsResultException, JsSuccess}
 import play.api.mvc.BodyParsers.parse
 import play.api.mvc.{Action, Results}
-
-import scala.collection.immutable.Iterable
 
 /**
   * Created by jameshsiao on 9/2/16.
   */
 @Singleton
-class SettingsController @Inject()(system: ActorSystem, emailService: EmailService) {
-
-  import scala.concurrent.ExecutionContext.Implicits.global
-  implicit val materializer = ActorMaterializer()(system)
+class SettingsController @Inject()(system: ActorSystem, materializer: Materializer, emailService: EmailService) {
 
   def handleForgotPassword = Action.async(parse.json) { request =>
-    val forgotPasswordSource = Source[ForgotPassword](Iterable(request.body.as[ForgotPassword]))
-    val response = forgotPasswordSource.map(forgot => emailService.sendForgotPasswordEmail(forgot)).via(serializeToJsonResponse).runWith(Sink.head)
-    response.map( result => Results.Status(result._1)(result._2) )
+    val forgotPasswordSource = Source.single[ForgotPassword](request.body.validate[ForgotPassword] match {
+      case js: JsSuccess[ForgotPassword] => js.get
+      case err: JsError => throw new JsResultException(err.errors)
+    })
+    forgotPasswordSource.map(forgot => emailService.sendForgotPasswordEmail(forgot))
+      .via(serializeToJsonResponse)
+      .map( result => Results.Status(result._1)(result._2) )
+      .runWith(Sink.head)(materializer)
+//    response
   }
 }
